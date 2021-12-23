67a5b2dc0fc0c345be2dc39fac2dd4aba6efc4e0
ytmschk <96598222+ytmschk@users.noreply.github.com>
2021-12-24 00:22:30 +0200

    Update TestTask.sql

--- TestTask.sql
+++ TestTask.sql
@@ -1,4 +1,4 @@
 SELECT a.category_name, AVERAGE (c.cost) AS average_cost 
 FROM adverts INNER JOIN costs ON a.id = c.id 
-WHERE average_cost > 500
+WHERE average_cost < 500
 GROUP BY a.category_name 
